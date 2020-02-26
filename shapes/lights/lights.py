4f4189a75fcee87467d532a5fee70375c48e9a6b
Ryan Spangler <ryan.spangler@gmail.com>
2020-02-25 22:14:25 -0800

    interactions with neighboring values

--- shapes/lights/lights.py
+++ shapes/lights/lights.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 from bibliopixel import animation
-from bibliopixel.colors import COLORS
+from bibliopixel.colors import COLORS, hsv2rgb
 
 """
 This is an example Animation class which has a single field -
@@ -12,19 +14,30 @@ write Python code in general.
 class Lights(animation.BaseAnimation):
     def __init__(
             self, *args,
-            # The fields for your class go here!
-            color=COLORS.green,
+            #################
 
-            # End of the fields for your class
+            # length=10,
+
+            #################
             **kwds):
-        super().__init__(*args, **kwds)
-        self.color = color
-        # Your initialization code goes here.
 
-    def step(self, amt=1):
+        super().__init__(*args, **kwds)
+
+        ###############################33
+
+        self.length = 144 # length
+        self.field = np.random.sample((self.length, 3))
+        self.increment_factor = 0.0
+        self.weight = np.array([0.0, 0.9, 0.1])
+        self.neighbor_kernel = np.repeat(self.weight, 3).reshape((3, 3))
+        self.field_increment = np.random.sample(
+            (self.length, 3)) * self.increment_factor - (0.5 * self.increment_factor)
+        self.flip_threshold = 0.05
+
+    def step_chase(self, amt=1):
         this_pixel = self.cur_step % len(self.color_list)
 
-        self.color_list[this_pixel] = self.color
+        self.color_list[this_pixel] = COLORS.goldenrod
         self.color_list[this_pixel - 1] = COLORS.black
 
         self.color_list[this_pixel - 5] = COLORS.green
@@ -33,6 +46,35 @@ class Lights(animation.BaseAnimation):
         self.color_list[this_pixel - 30] = COLORS.blue
         self.color_list[this_pixel - 41] = COLORS.red
         self.color_list[this_pixel - 55] = COLORS.black
+
+    def step_thing(self, amt=1):
+        if np.random.sample() < self.flip_threshold:
+            flip = np.random.choice(self.length)
+            self.field[flip] += 0.5
+
+        self.field += self.field_increment
+        self.field[np.where(self.field < 0)] += 1.0
+        self.field[np.where(self.field > 1.0)] -= 1.0
+        next_field = np.zeros((self.length, 3))
+
+        for pixel in range(self.length):
+            hsv = self.field[pixel]
+            before = self.field[(pixel - 1) % self.length]
+            after = self.field[(pixel + 1) % self.length]
+            neighbors = np.array([before, hsv, after])
+            become = np.sum(neighbors * self.neighbor_kernel, axis=0)
+            next_field[pixel] = become
+            
+            rgb = hsv2rgb((
+                int(become[0] * 255),
+                int(become[1] * 255),
+                int(become[2] * 255)))
+            self.color_list[pixel] = rgb
+
+        self.field = next_field
+
+    def step(self, amt=1):
+        self.step_thing(amt)
 
     # pre_run is called right before the animation starts running.
     def pre_run(self):

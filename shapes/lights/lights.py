1db7529e26dc8605fbb8be8891eec965dc58de34
Ryan Spangler <ryan.spangler@gmail.com>
2020-03-22 17:31:31 -0700

    added vertical associations, moving point

--- shapes/lights/lights.py
+++ shapes/lights/lights.py
@@ -25,14 +25,48 @@ class Lights(animation.BaseAnimation):
 
         ###############################33
 
+        self.mode = 'point'
+
         self.length = 144 # length
-        self.field = np.random.sample((self.length, 3))
+
+        # self.field = np.random.sample((self.length, 3))
+        self.field = np.zeros((self.length, 3))
         self.increment_factor = 0.0
         self.weight = np.array([0.0, 0.9, 0.1])
         self.neighbor_kernel = np.repeat(self.weight, 3).reshape((3, 3))
         self.field_increment = np.random.sample(
             (self.length, 3)) * self.increment_factor - (0.5 * self.increment_factor)
         self.flip_threshold = 0.05
+
+        ################################33
+
+        self.previous_point = 0
+        self.point = np.random.randint(self.length)
+        self.point_max = np.array([1.0, 1.0, 1.0])
+        self.vertical = 41
+        self.vertical_next = self.vertical + 1
+        self.directions = [
+            -1, 1,
+            -self.vertical, -self.vertical_next,
+            self.vertical, self.vertical_next]
+
+        self.delay = 10
+        self.cycle = self.delay
+        self.delta = (1.0 / self.delay) * np.array([0.0, 0.0, 1.0])
+
+        self.goal = self.point
+
+        if self.mode == 'point':
+            self.field[self.point][:] = (1.0, 1.0, 1.0)
+
+    def render_field(self):
+        for pixel in range(self.length):
+            hsv = self.field[pixel]
+            rgb = hsv2rgb((
+                int(hsv[0] * 255),
+                int(hsv[1] * 255),
+                int(hsv[2] * 255)))
+            self.color_list[pixel] = rgb
 
     def step_chase(self, amt=1):
         this_pixel = self.cur_step % len(self.color_list)
@@ -69,17 +103,53 @@ class Lights(animation.BaseAnimation):
             become = np.sum(neighbors * self.neighbor_kernel, axis=0)
             next_field[pixel] = become
             
-            rgb = hsv2rgb((
-                int(become[0] * 255),
-                int(become[1] * 255),
-                int(become[2] * 255)))
-            self.color_list[pixel] = rgb
-
         self.field = next_field
 
+        self.render_field()
+
+    def step_decimal(self, amt=1):
+        for pixel in range(self.length):
+            if pixel % 42 == 6:
+                b = 128
+                brightness = (b, b, b)
+                self.color_list[pixel] = brightness
+
+    def step_point(self, amt=1):
+        if self.cycle < self.delay:
+            self.cycle += 1
+            self.field[self.point][:] -= self.delta
+            self.field[self.goal][:] += self.delta
+        else:
+            self.cycle = 0
+
+            self.field[self.point][:] = np.zeros(self.point_max.shape)
+            self.point = self.goal
+            self.field[self.point][:] = self.point_max
+
+            valid_directions = [
+                self.point + direction
+                for direction in self.directions
+                if self.point + direction >= 0 and self.point + direction < self.length]
+
+            self.goal = np.random.choice(valid_directions)
+            self.field[self.goal][:] = (1.0, 1.0, 0.0)
+
+
+            # self.color_list[self.point] = (0, 0, 0)
+            # self.color_list[choice] = (127, 127, 127)
+            # self.point = choice
+
+        self.render_field()
+
     def step(self, amt=1):
-        # self.step_chase(amt)
-        self.step_thing(amt)
+        if self.mode == 'point':
+            self.step_point(amt)
+        elif self.mode == 'decimal':
+            self.step_decimal(amt)
+        elif self.mode == 'chase':
+            self.step_chase(amt)
+        elif self.mode == 'thing':
+            self.step_thing(amt)
 
     # pre_run is called right before the animation starts running.
     def pre_run(self):
